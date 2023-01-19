501bd709875047f0d7cb4423b6c2785fb0854b21
KOLESNiii <timkolesnichenko05@gmail.com>
2023-01-19 22:39:11 +0000

    Added orbit module (cannot be installed through pip) Added geotagging for photos and future support for CSV data logging
    Changed function parameters for cleaner passing of camera object
    	modified:   main.py
    	new file:   orbit/__init__.py
    	new file:   orbit/__pycache__/__init__.cpython-310.pyc
    	new file:   orbit/__pycache__/motion_sensor.cpython-310.pyc
    	new file:   orbit/__pycache__/telemetry.cpython-310.pyc
    	new file:   orbit/telemetry.py

--- main.py
+++ main.py
@@ -6,25 +6,46 @@ except ImportError:
     cameraExists = False
 from time import sleep, time
 from os import makedirs, path
+from orbit import ISS
+from pathlib import Path
 
-if cameraExists:
-    camera = PiCamera()
-    camera.resolution = (4056, 3040)
+
     
 TIMEBETWEENPHOTOS = 40 #seconds  
 RUNNINGTIME = 180 #minutes-- ideally closer to 170 for actual use to be safe
 TESTING = True #testing mode-- artificial environments to test certain functions
+PROGRAMPATH = Path(__file__).parent.resolve() #path to this file
 
 def MakeDirs():
     '''Makes the folders for images and other save files.'''
-    makedirs('./images/', exist_ok = True)
+    makedirs(f'{PROGRAMPATH}/images/', exist_ok = True)
     
+def ConvertCoordinates(angle):
+    '''
+    Converts a 'skyfield' angle to a string of degrees, minutes, and seconds.
+    
+    Returns a string of the converted angle.
+    '''        
+    sign, degrees, minutes, seconds = angle.signed_dms()
+    coordinate = degrees + minutes/60 + seconds/3600
+    coordinate *= sign
+    return str(coordinate)
 
-def TakePicture(imgCount):
+def TakePicture(camera, imgCount):
     '''Takes a picture and saves it to the current directory.'''
+    
+    currentLocation = ISS.coordinates()
+    latitude = ConvertCoordinates(currentLocation.latitude)
+    longitude = ConvertCoordinates(currentLocation.longitude)
+    #TODO:
+    #add this to CSV file along with timestamp
     if cameraExists:
-        camera.capture(f'./images/image_{imgCount}.jpg')
-        sleep(2)
+        camera.exif_tags['GPS.GPSLatitude'] = latitude #cannot store signed numbers in these fields
+        camera.exif_tags['GPS.GPSLongitude'] = longitude
+        camera.exif_tags['GPS.GPSLatitudeRef'] = 'S' if currentLocation.latitude.degrees < 0 else 'N' #if negative, is S
+        camera.exif_tags['GPS.GPSLongitudeRef'] = 'W' if currentLocation.longitude.degrees < 0 else 'E' #if negative, is W
+        camera.capture(f'{PROGRAMPATH}/images/image_{imgCount}.jpg')
+        sleep(2) #camera processing time
     else:
         print(f'image_{imgCount}.jpg')
     imgCount += 1
@@ -37,15 +58,21 @@ def GetNumberString(int, targetLen = 3):
         string = '0' + string
     return string
 
-def Finish():
+def Finish(camera):
     '''Closes the camera and ends the program.'''
     if cameraExists:
         camera.close()
     exit()
         
 def main():
-    if not TESTING and not cameraExists: #If the camera module does not work and we are not testing locally
+    if cameraExists:
+        camera = PiCamera()
+        camera.resolution = (4056, 3040)
+        sleep(1) #camera warmup
+    elif not TESTING: #If the camera module does not work and we are not testing locally
         raise ImportError("Camera module could not be imported. Exiting.")
+    else:
+        camera = None
     
     MakeDirs()
     imageCount = 0
@@ -54,17 +81,17 @@ def main():
         timeVirtual = 0
         while timeVirtual < RUNNINGTIME*60: #90 virtual minutes
             if timeVirtual % TIMEBETWEENPHOTOS == 0:
-                imageCount = TakePicture(imageCount)
+                imageCount = TakePicture(camera, imageCount)
             timeVirtual += 1
     else:
         lastTime = time()
         startTime = time()
         while time() - startTime <= RUNNINGTIME*60: #90 minutes
             if time() - lastTime >= TIMEBETWEENPHOTOS:
-                imageCount = TakePicture(imageCount)
+                imageCount = TakePicture(camera, imageCount)
                 lastTime = time()
         
-    Finish()
+    Finish(camera)
     
 main()
         
