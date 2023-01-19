6983acfc2ef464d1af0c8817d5e7122ce974e706
KOLESNiii <timkolesnichenko05@gmail.com>
2023-01-19 09:44:44 +0000

    Made the photo function loop Made photo number string function 	modified:   main.py

--- main.py
+++ main.py
@@ -8,13 +8,33 @@ from time import sleep, time
 
 if cameraExists:
     camera = PiCamera()
-imgCount = 0
+    
+TIMEBETWEENPHOTOS = 40 #seconds    
+
 def TakePicture(imgCount):
     '''Takes a picture and saves it to the current directory.'''
     if cameraExists:
         camera.capture(f'image_{imgCount}.jpg')
+    else:
+        print(f'image_{imgCount}.jpg')
     imgCount += 1
     return imgCount
+
+def getNumberString(int, targetLen = 3):
+    '''Returns a string of the number with leading zeros if needed.'''
+    string = str(int)
+    while len(string) < targetLen:
+        string = '0' + string
+    return string
+        
+def main():
+    timeVirtual = 0
+    imageCount = 0
+    for i in range(90*60):
+        if timeVirtual % TIMEBETWEENPHOTOS == 0:
+            imageCount = TakePicture(imageCount)
+        timeVirtual += 1
+        
 
 
     
