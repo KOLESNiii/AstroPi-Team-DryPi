4f156a9caef84366eea09fd946b41e86874bbbdc
KOLESNiii <timkolesnichenko05@gmail.com>
2023-02-20 22:18:17 +0000

    Added CSV module, to track location and images and times 	modified:   main.py

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
+from time import sleep, time, strftime, gmtime
+from os import makedirs, path
+from orbit import ISS
+from pathlib import Path
+import logging, csv
 try:
     from picamera import PiCamera
     cameraExists = True
 except ImportError:
     logging.warning("Camera module could not be imported on this device.")
     cameraExists = False
-from time import sleep, time
-from os import makedirs, path
-from orbit import ISS
-from pathlib import Path
-import logging
     
 TIMEBETWEENPHOTOS = 40 #seconds  
+TIMEBETWEENDATALOGS = 10 #seconds
 RUNNINGTIME = 180 #minutes-- ideally closer to 170 for actual use to be safe
 TESTING = True #testing mode-- artificial environments to test certain functions
+VIRTUALTIMEGAP = 0.1 #second gap between each virtual "second" in testing mode
 PROGRAMPATH = Path(__file__).parent.resolve() #path to this file
 logging.basicConfig(filename=f'{PROGRAMPATH}/all.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+def addCSVEntry(imageTaken = False, imgNum = None):
+    '''Adds a row to the CSV file with the given data.'''
+    latitude, longitude, _ = getLocation()
+    latitude, longitude = float(latitude), float(longitude)
+    time = strftime("%H:%M:%S", gmtime())
+
+    with open(f'{PROGRAMPATH}/data.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([imageTaken, imgNum, latitude, longitude, time])
+
 
 def MakeDirs():
     '''Makes the folders for images and other save files.'''
     makedirs(f'{PROGRAMPATH}/images/', exist_ok = True)
+    with open(f'{PROGRAMPATH}/data.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Image Taken', 'Image Number', 'Latitude', 'Longitude', 'Time'])
     logging.info("Created directories for images and other save files.")
     
 def ConvertCoordinates(angle):
@@ -32,19 +48,28 @@ def ConvertCoordinates(angle):
     coordinate *= sign
     return str(coordinate)
 
-def TakePicture(camera, imgCount):
-    '''Takes a picture and saves it to the current directory.'''
-    
+def getLocation(advanced = False):
+    '''Returns the current location of the ISS as a tuple of (latitude, longitude).'''
     currentLocation = ISS.coordinates()
     latitude = ConvertCoordinates(currentLocation.latitude)
     longitude = ConvertCoordinates(currentLocation.longitude)
-    #TODO:
-    #add this to CSV file along with timestamp
+    if advanced:
+        angles = (currentLocation.latitude.degrees, currentLocation.longitude.degrees)
+    else:
+        angles = None
+    return (latitude, longitude, angles)
+
+def TakePicture(camera, imgCount):
+    '''Takes a picture and saves it to the current directory.'''
+    
+    latitude, longitude, angles = getLocation(True)
+    latAngle, longAngle = angles
+
     if cameraExists:
         camera.exif_tags['GPS.GPSLatitude'] = latitude #cannot store signed numbers in these fields
         camera.exif_tags['GPS.GPSLongitude'] = longitude
-        camera.exif_tags['GPS.GPSLatitudeRef'] = 'S' if currentLocation.latitude.degrees < 0 else 'N' #if negative, is S
-        camera.exif_tags['GPS.GPSLongitudeRef'] = 'W' if currentLocation.longitude.degrees < 0 else 'E' #if negative, is W
+        camera.exif_tags['GPS.GPSLatitudeRef'] = 'S' if latAngle < 0 else 'N' #if negative, is S
+        camera.exif_tags['GPS.GPSLongitudeRef'] = 'W' if longAngle < 0 else 'E' #if negative, is W
         camera.capture(f'{PROGRAMPATH}/images/image_{imgCount}.jpg')
         sleep(2) #camera processing time
     else:
@@ -85,18 +110,29 @@ def main():
     
     if TESTING:
         logging.info("Running in testing mode.")
+        logging.warning("Will run for " + str(RUNNINGTIME*VIRTUALTIMEGAP) + " minutes.")
+        logging.warning("Process should terminate at " + strftime("%H:%M:%S", gmtime(time() + RUNNINGTIME*VIRTUALTIMEGAP*60)) + ".")
         timeVirtual = 0
-        while timeVirtual < RUNNINGTIME*60: #90 virtual minutes
-            if timeVirtual % TIMEBETWEENPHOTOS == 0:
+        while timeVirtual < RUNNINGTIME*60: #180 virtual minutes
+            if timeVirtual % TIMEBETWEENPHOTOS == 0:  
                 imageCount = TakePicture(camera, imageCount)
+                addCSVEntry(True, imageCount)
+            if timeVirtual % TIMEBETWEENDATALOGS == 0:
+                addCSVEntry(imgNum=imageCount)
             timeVirtual += 1
+            sleep(VIRTUALTIMEGAP)
     else:
-        lastTime = time()
+        lastImageTime = time()
+        lastDataLogTime = time()
         startTime = time()
-        while time() - startTime <= RUNNINGTIME*60: #90 minutes
-            if time() - lastTime >= TIMEBETWEENPHOTOS:
+        while time() - startTime <= RUNNINGTIME*60: #180 minutes
+            if time() - lastImageTime >= TIMEBETWEENPHOTOS:
                 imageCount = TakePicture(camera, imageCount)
-                lastTime = time()
+                addCSVEntry(True, imageCount)
+                lastImageTime = time()
+            if time() - lastDataLogTime >= TIMEBETWEENDATALOGS:
+                addCSVEntry(imgNum=imageCount)
+                lastDataLogTime = time()
         
     Finish(camera)
     
